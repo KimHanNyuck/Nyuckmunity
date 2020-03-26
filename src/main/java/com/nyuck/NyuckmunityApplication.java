f08b9363d2ac22edb913060219c74a3ba239738f
KimHanNyuck <revubplay@nate.com>
2020-03-26 20:08:41 +0900

    Create

---
+++ src/main/java/com/nyuck/NyuckmunityApplication.java
@@ -0,0 +1,13 @@
+package com.nyuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyuckmunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyuckmunityApplication.class, args);
+	}
+
+}
